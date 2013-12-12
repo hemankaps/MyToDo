0176323a083f2bb4958f985cbc14365fde28f658
hemankaps <himanshu.kapse@gmail.com>
2013-12-12 02:13:05 -0800

    Added another activity and new functionality to persist the changes

--- src/com/codepath/apps/mytodo/TodoActivity.java
+++ src/com/codepath/apps/mytodo/TodoActivity.java
@@ -1,16 +1,18 @@
 package com.codepath.apps.mytodo;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import com.codepath.apps.mytodo.utils.FileUtils;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -22,6 +24,7 @@ public class TodoActivity extends Activity {
 	ArrayAdapter<String> itemsAdapter;
 	ListView lvItems;
 	private static final String fileName = "todo.txt";
+	private final int REQUEST_CODE = 200;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +47,10 @@ public class TodoActivity extends Activity {
         return true;
     }
     
+    /**
+     * This method will add the new entry to arraylist.
+     * @param v
+     */
     public void addToDoItem(View v){
     	EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
     	String newStr = etNewItem.getText().toString().trim();
@@ -51,9 +58,15 @@ public class TodoActivity extends Activity {
     	saveItems(newStr);
     	etNewItem.setText("");
     }
-    
-    private void setupListViewListener(){
-    	lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+   
+
+    /**
+     * This method is listener to the long click event on the list item.
+     * It will remove the item from the list.
+     */
+	private void setupListViewListener(){
+    	//Long click listener for the list item
+		lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
     		@Override
     		public boolean onItemLongClick(AdapterView<?> aView, View item, int pos, long id){
     			items.remove(pos);
@@ -63,16 +76,48 @@ public class TodoActivity extends Activity {
     	}
       );
     	
+    //Onclick listener for the list item to make it editable
+	lvItems.setOnItemClickListener(new OnItemClickListener() {
+
+		@Override
+		public void onItemClick(AdapterView<?> arg0, View item, int pos,
+				long id) {
+			//Create an intent
+			Intent intent = new Intent(TodoActivity.this, EditItemListActivity.class);
+			
+			//open Edit activity and pass on the the data of the item			
+			intent.putExtra("editText", items.get(pos));
+			intent.putExtra("itemPos", pos);
+			//Launch other activity, here Edit activity.
+			startActivityForResult(intent, REQUEST_CODE);
+		}
+		
+	}
+	);
+    	
     }
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	  // REQUEST_CODE is defined above
+	  if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+	     // Extract name value from result extras
+	     String etText = data.getExtras().getString("editText");
+	     int itemPos = data.getIntExtra("itemPos", 0);
+	     items.remove(itemPos);
+	     items.add(itemPos, etText);
+	     itemsAdapter.notifyDataSetInvalidated();
+	     updateFinalList();
+	     
+	  }
+	} 
     
+	/**
+	 * This method reads the last saved data from the disk, to be displayed to user.
+	 */
     
     private void readItems(){
     	File filesDir = getFilesDir();
-    	System.out.println("FilesDir :: " + filesDir);
-        //Check the file in desired location
-    	
-    	
-    	//Create new file only if it does not exist
     	File todoFile = new File(filesDir, fileName);
     	try{
     		
@@ -83,22 +128,62 @@ public class TodoActivity extends Activity {
     		}
     		
     		
-    	}catch(Exception ex){
+    	}catch(IOException ex){
     		
     	}
     	
     }
     
+    /**
+     * This method saves the new entry added to the list on the disk.
+     * @param newText
+     */
     
     private void saveItems(String newText){
     	File filesDir = getFilesDir();
-    	System.out.println("FilesDir :: " + filesDir);
     	File todoFile = new File(filesDir,fileName);
-    	System.out.println("File to be created :: " + todoFile.toString());
     	try{
     		FileUtils.writeLines(todoFile, newText);
+    	} catch(Exception ex){
+    		System.out.println(ex.getMessage());
+    	}
+    }
+    
+    /**
+     * This method will be called to Save the current list on user screen.
+     * This will persist the latest list on disk and discard the old one.
+     * @param v
+     */
+    
+    public void saveFinalList(View v){
+    	
+    	updateFinalList();
+    }
+    
+    /**
+     * Rewrite list in persistent store
+     */
+     private void updateFinalList(){
+    	//Delete the file
+     	deleteList();
+     	for (String lsText: items) {
+ 			saveItems(lsText);
+ 		}
+     }
+    /**
+     * This method will delete the list entries from disk
+     */
+    private void deleteList() {
+		// call delete file function
+    	File filesDir = getFilesDir();
+    	File todoFile = new File (filesDir,fileName);
+    	try{
+    		
+    		 FileUtils.deleteLines(todoFile);
+    		
     	}catch(Exception ex){
     		
     	}
-    }
+		
+	}
 }
--- src/com/codepath/apps/mytodo/utils/FileUtils.java
+++ src/com/codepath/apps/mytodo/utils/FileUtils.java
@@ -1,18 +1,13 @@
 package com.codepath.apps.mytodo.utils;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
@@ -20,32 +15,34 @@ public class FileUtils {
 
 	
 	private static final boolean MODE_APPEND = true;
-	private static final String fileName = "todo.txt";
-
-	public static ArrayList<String> readLines(File fileName){
+	private static BufferedReader buffreader;
+	
+	/**
+	 * This method reads data from the given file and returns a list.
+	 * @param fileName
+	 * @return
+	 */
+	public static ArrayList<String> readLines(File fileName) throws FileNotFoundException{
 	  
 		
 		ArrayList<String> lvItems = new ArrayList<String>();
-	
+		
+		//check if given file exists
 		if(fileName.exists()){
-			FileInputStream fis = null;
 			
 			try{
-				 fis = new FileInputStream(fileName);
-				 // open the file for reading we have to surround it with a try
-			       
-		            InputStream instream = new FileInputStream(fileName);//open the text file for reading
-		           
-		            // if file the available for reading
+				 
+				 InputStream instream = new FileInputStream(fileName);	 
+			
 		            if (instream != null) {               
 		               
 		            	// prepare the file for reading
 		            	InputStreamReader inputreader = new InputStreamReader(instream);
-		            	BufferedReader buffreader = new BufferedReader(inputreader);
-		              
+		            	buffreader = new BufferedReader(inputreader);
+		            	
+		    
 		            	String line=null;
-		            	//We initialize a string "line"
-		             
+		            
 			            while (( line = buffreader.readLine()) != null) {
 			                //buffered reader reads only one line at a time, hence we give a while loop to read all till the text is null
 			                                       
@@ -53,51 +50,57 @@ public class FileUtils {
 			            }
 		          }
 		                                     
-				} catch(IOException ioex){
-					System.out.println("File Does not Exist :: " + ioex.getMessage());
+				} 
+				 catch(IOException ioex){
+					System.out.println("Exception :: " + ioex.getMessage());
+					//Throw this exception to the caller
 				  }
-			
-			
+			} else {
+				throw new FileNotFoundException("File does not Exist");
 			}
 		return lvItems;
 	  }
 	
+	/**
+	 * This method appends the given text to the given file.
+	 * @param todo
+	 * @param text
+	 */
 	public static void writeLines(File todo, String text) {
-		// TODO Auto-generated method stub
-		
-		//File todoFile = new File(fileName);
+	    //Check if data is not null
 		if(text != null){
 			try{
-				System.out.println("If file exist :: " + todo.exists());
+				//check if the file exist, else create a new
 			    if(!todo.exists()){
 			    	todo.createNewFile();
-			    	System.out.println("After creating, file exist :: " + todo.exists());
 			    	todo.setWritable(true);
-			    	
-			    }
+			    } 
 					
-				// open myfilename.txt for writing
 		          OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(todo,MODE_APPEND));
 		          
-		          // write the contents to the file
-		          //System.out.println("Size of the Array :: " + items.size());
-		          
-		          //for(String str: items){
 		           out.write(text);
 		           out.write('\n');
-		           System.out.println("Added :: " + text + " to the file.");
-		          //}
-		          // close the file
-		          out.close();
+		           out.close();
 			}
-			catch(FileNotFoundException fex){
-				System.out.println("File not found ::" + fex.getMessage());
-			}
-			
 			catch(IOException ioe){
 				System.out.println("Exception in File Utils :: " + ioe.getMessage());
+				//Throw this exception to the caller
 			}
 		}
+	}
+
+	/**
+	 * Deletes the file from the disk.
+	 * @param todoFile
+	 */
+	public static void deleteLines(File todoFile) throws FileNotFoundException {
+		// delete the file
+		if(todoFile.exists()){
+			todoFile.delete();
+		} else {
+			throw new FileNotFoundException("File does not Exist");
+		}
+		
 	}
 	
 }
